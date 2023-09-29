5b2b8f773ec13336e651a947ac98f9b7e5651c9d
ttteety <ttteety@yahoo.com>
2023-09-29 21:26:09 +1000

    Added logging

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import uuid
 import os 
 import glob
 import pandas as pd
+import logging
 
 
-# Folder parhs are hard coded
-# shcemas.json path is also hard coded
+
 def get_columns(ds):
     schema_file_path = os.environ.setdefault('SCHEMAS_FILE_PATH', 'data/retail_db/schemas.json')
     with open(schema_file_path) as fp:
@@ -18,27 +18,35 @@ def get_columns(ds):
         cols = sorted(schema, key=lambda s: s['column_position'])
         columns = [col['column_name'] for col in cols]
         return columns 
-    except KeyError:
-        print(f'Schema not found for {ds}')
-        return
+    except KeyError as ke:
+        logging.error(f'Schema not found for {ds}')
+        raise
 
 
 def process_file(src_base_dir, ds, tgt_base_dir):
     for file in glob.glob(f'{src_base_dir}/{ds}/part*'):
-        df = pd.read_csv(file, names=get_columns(ds))
-        os.makedirs(f'{tgt_base_dir}/{ds}', exist_ok=True)
-        df.to_json(
-            f'{tgt_base_dir}/{ds}/part-{str(uuid.uuid1())}.json',
-            orient='records',
-            lines=True
-        )
-        print(f'Number of records processed for {os.path.split(file)[1]} is {ds} is {df.shape[0]}')
-
+        try:
+            df = pd.read_csv(file, names=get_columns(ds))
+            os.makedirs(f'{tgt_base_dir}/{ds}', exist_ok=True)
+            df.to_json(
+                f'{tgt_base_dir}/{ds}/part-{str(uuid.uuid1())}.json',
+                orient='records',
+                lines=True
+            )
+            logging.info(f'Number of records processed for {os.path.split(file)[1]} is {ds} is {df.shape[0]}')
+        except KeyError:
+            raise
 
 def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        filename='logs/ffc.log',
+        format='%(levelname)s %(asctime)s %(message)s'
+    )
     src_base_dir = os.environ['SRC_BASE_DIR']
     tgt_base_dir = os.environ['TGT_BASE_DIR']
     datasets = os.environ.get('DATASETS')
+    logging.info('File format conversion: Successful')
     if not datasets:
         for path in glob.glob(f'{src_base_dir}/*'):
             if os.path.isdir(path):
@@ -46,7 +54,11 @@ def main():
     else:
         dirs = datasets.split(',')
         for ds in dirs:
-            process_file(src_base_dir, ds, tgt_base_dir)    
+            try:
+                process_file(src_base_dir, ds, tgt_base_dir)    
+            except Exception as e:
+                logging.error(f'File format conversion for {ds} is not successful')
+    logging.info('File format conversion: Successful')
 
 if __name__ == "__main__":
     main()
